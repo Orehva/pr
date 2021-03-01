7d83a47a8e2689473f1f50eb65dd9435481756cf
Evgen <orehva@tut.by>
2021-03-01 15:19:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const firstName= 'Evgeniy';
+const lastName= 'Orehva';
+const age=30;
+const str= 'Hello my name is Evgeniy';
+let value;
+value=firstName+lastName;
+
+value=firstName+' '+ lastName;
+
+
+
+
+console.log(value);
